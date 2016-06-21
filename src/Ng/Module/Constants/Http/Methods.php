51400d2f6218dedde1a858a9b7566fe7af2cdaa6
Ady Rahmat MA <ngurajeka.92@gmail.com>
2016-06-21 19:11:30 +0700

    Constant
    Using static method to get all methods.

--- src/Ng/Module/Constants/Http/Methods.php
+++ src/Ng/Module/Constants/Http/Methods.php
@@ -4,11 +4,30 @@ namespace Ng\Module\Constants\Http;
 
 class Methods
 {
-    const METHODS   = ["GET", "PATCH", "PUT", "POST", "DELETE", "OPTIONS"];
     const GET       = "GET";
+    const HEAD      = "HEAD";
     const POST      = "POST";
     const PUT       = "PUT";
     const PATCH     = "PATCH";
     const DEL       = "DELETE";
     const OPT       = "OPTIONS";
+
+    public static function getMethods($string=false)
+    {
+        $methods = array(
+            self::GET,
+            self::PATCH,
+            self::PUT,
+            self::POST,
+            self::DEL,
+            self::OPT,
+        );
+
+        if ($string) {
+           return join(",", $methods);
+        }
+
+        return $methods;
+    }
+
 }
